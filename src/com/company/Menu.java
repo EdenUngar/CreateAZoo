0e153cd271434ad88414f6ef50d854ff911002ea
Eden Ungar <edenungar@gmail.com>
2018-06-08 13:31:04 -0400

    added Manage People options

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -14,7 +14,8 @@ public class Menu {
             System.out.println("Please choose an option. " +
                     "\n1. Manage People" +
                     "\n2. Manage Animals" +
-                    "\n3. Manage Inventory");
+                    "\n3. Manage Inventory" +
+                    "\n4. Exit Program");
 
             //handles user input for navigation
             switch (input.nextInt()) {
@@ -30,10 +31,15 @@ public class Menu {
                     //Manage Inventory
                     manageInventory();
                     break;
+                case 4:
+                    System.out.println("Thank you for using the Zoo Program");
+                    //exit program
+                    System.exit(0);
+                    break;
                 default:
                     //handle wrong number
                     //tell them they did something wrong
-                    System.out.println("Not a valid entry. Please enter a number between 1 and 3. ");
+                    System.out.println("Not a valid entry. Please enter a number between 1 and 4. ");
                     //restart method to allow user to try again
                     mainMenu();
                     break;
@@ -41,7 +47,7 @@ public class Menu {
         } catch (InputMismatchException ime){
             //handle if user puts in something that is not an int
             input.nextLine();
-            System.out.println("Not a valid entry. Please enter a number between 1 and 3. ");
+            System.out.println("Not a valid entry. Please enter a number between 1 and 4. ");
             //restart method to allow user to try again
             mainMenu();
 
@@ -49,16 +55,64 @@ public class Menu {
 
     }
 
+
     //handles People Options
     private void managePeople() {
 
         try{
             //enter code you would like to run that MAY throw an exception
+            System.out.println("Please choose an option. " +
+                    "\n1. Add People" +
+                    "\n2. View People" +
+                    "\n3. Remove People" +
+                    "\n4. Back to Main Menu" +
+                    "\n5. Exit Program");
+            switch (input.nextInt()){
+                case 1:
+                    //add people
+                    addPeople();
+                    break;
+                case 2:
+                    //view people
+                    viewPeople();
+                    break;
+                case 3:
+                    //remove people
+                    removePeople();
+                    break;
+                case 4:
+                    //back to main menu
+                    mainMenu();
+                    break;
+                case 5:
+                    //exit program
+                    System.out.println("Thank you for using the Zoo Program");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Not a valid entry. Please enter a number between 1 and 5. ");
+                    managePeople();
+                    break;
+            }
         } catch (InputMismatchException ime){
             //what you want it to do IF this exception happens
+            input.nextLine();
+            System.out.println("Not a valid entry. Please enter a number between 1 and 5.");
+            managePeople();
         }
 
     }
+
+    private void addPeople() {
+    }
+    private void viewPeople() {
+    }
+    private void removePeople() {
+    }
+
+
+
+
 
     //handles Animal Options
     private void manageAnimals() {
@@ -67,7 +121,6 @@ public class Menu {
     //handles Inventory Options
     private void manageInventory() {
     }
-
 
 
 }
