69872ba4d46e1bfe5dd4193b9ffea7b9316a5f57
Eden Ungar <edenungar@gmail.com>
2018-06-08 12:15:32 -0400

    added switch and created a catch for invalid options

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -9,15 +10,64 @@ public class Menu {
 
     public void mainMenu(){
 
+        try {
+            System.out.println("Please choose an option. " +
+                    "\n1. Manage People" +
+                    "\n2. Manage Animals" +
+                    "\n3. Manage Inventory");
 
-        System.out.println("Please choose an option. " +
-                "\n1. Manage People" +
-                "\n2. Manage Animals" +
-                "\n3. Manage Inventory");
+            //handles user input for navigation
+            switch (input.nextInt()) {
+                case 1:
+                    //Manage People
+                    managePeople();
+                    break;
+                case 2:
+                    //Manage Animals
+                    manageAnimals();
+                    break;
+                case 3:
+                    //Manage Inventory
+                    manageInventory();
+                    break;
+                default:
+                    //handle wrong number
+                    //tell them they did something wrong
+                    System.out.println("Not a valid entry. Please enter a number between 1 and 3. ");
+                    //restart method to allow user to try again
+                    mainMenu();
+                    break;
+            }
+        } catch (InputMismatchException ime){
+            //handle if user puts in something that is not an int
+            input.nextLine();
+            System.out.println("Not a valid entry. Please enter a number between 1 and 3. ");
+            //restart method to allow user to try again
+            mainMenu();
 
-        //handles user input for navigation
+        }
 
     }
+
+    //handles People Options
+    private void managePeople() {
+
+        try{
+            //enter code you would like to run that MAY throw an exception
+        } catch (InputMismatchException ime){
+            //what you want it to do IF this exception happens
+        }
+
+    }
+
+    //handles Animal Options
+    private void manageAnimals() {
+    }
+
+    //handles Inventory Options
+    private void manageInventory() {
+    }
+
 
 
 }
